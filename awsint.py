3256a8446e19171f9b8cb1f5c98fcaf710fb2e8e
sravas44 <panchireddysravani@gmail.com>
2025-07-02 13:04:00 +0530

    Update awsint.py

--- awsint.py
+++ awsint.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-dynamodb = boto3.resource('dynamodb', region_name = "region_name")
+dynamodb = boto3.resource('dynamodb', region_name = "us-east-1")
 photographers_table = dynamodb.Table('dynamodb_table_name')
 
 @app.route('/photographers', methods = ['GET'])
